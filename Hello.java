e6f115b8f47773a9525c8915dcd951469522afa0
harshith010 <harshithks94@gmail.com>
2024-07-22 20:50:52 +0530

    Updat code

--- Hello.java
+++ Hello.java
@@ -1,13 +1,21 @@
+class demo {
+    static void Hars(int a, int b) {
+        try {
+            int c = a + b;
+            if (c == 11) {
+                Thread.sleep(30);
+                System.out.println("============Program success============");
+            } else {
+                System.out.println("Failure");
+            }
+        } catch (Exception e) {
+            System.out.println("Retry");
+        }
+    }
+}
+
 public class Hello {
     public static void main(String[] args) {
-        demo.Hars(6,5);
+        demo.Hars(6, 5);
     }
-    }
-
-class demo {
-    
-    static void Hars(int a,int b) {
-        int c = a+b;
-        System.out.println(c);
-}
 }
