5d00a75a5c56de7774134a425640f25d585a276e
harshith010 <harshithks94@gmail.com>
2024-07-22 17:53:46 +0530

    adding java program

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello EveryOne*******************");
+    static void demo(int a,int b) {
+        int c = a+b;
+        System.out.println(c);
+    }   
+    }
 
+class demo {
+    public static void main(String[] args) {
+        Hello.demo(7,5);
     }
 }
